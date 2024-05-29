72e4b2eeaf6adfdd6a81c85afcfb7a52b8403e22
rod-driscoll <rod@theavitgroup.com.au>
2024-05-29 12:55:49 +1000

    removed unneccessary dependencies

--- 3 Series/src/Properties/AssemblyInfo.cs
+++ 3 Series/src/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("Navitas")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("Navitas-v1.0.0")]
+[assembly: AssemblyCompany("AVPlus Integration Pty Ltd")]
 [assembly: AssemblyProduct("Navitas")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyVersion("1.0.0.*")]
